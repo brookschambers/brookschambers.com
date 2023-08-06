f25d31ac6118986119688786a4cce624bafabf21
Drew Powers <drew@pow.rs>
2023-08-06 00:05:53 -0600

    Add Sitemap & robots.txt

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import { imagetools } from 'vite-imagetools';
 import lqip from 'vite-plugin-lqip';
 
 export default defineConfig({
 	site: 'https://brookschambers.com',
+	integrations: [sitemap()],
 	vite: {
 		plugins: [
 			lqip(),
